60bfab8d22e5ac8b314057be4bfc18db017392d7
Martin Palmowski <martin.palmowski@gmail.com>
2021-08-29 20:17:11 +0200

    Some better keybinds.

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g -- a table to access global variables
 local opt = vim.opt -- to set options
+local map = vim.api.nvim_set_keymap -- map keys easier
 
 
 -- Bootstrap Paq when needed ------------------------------------------
@@ -19,9 +20,10 @@ require("paq") ({
     "sainnhe/everforest",    -- colorscheme
     "folke/tokyonight.nvim", -- colorscheme
 
-
     "kyazdani42/nvim-web-devicons",
-    "ryanoasis/vim-devicons"
+    "ryanoasis/vim-devicons",
+
+    "kyazdani42/nvim-tree.lua",
   
 })
 
@@ -32,7 +34,13 @@ require("paq") ({
 })--]]
 
 
+-- colorscheme --------------------------------------------------------
 cmd([[colorscheme tokyonight]]) -- Put your favorite colorscheme here
+
+-- basics -------------------------------------------------------------
+g.mapleader = " "
+map('n', '<Leader>w', ':write<CR>', {noremap = true}) -- quicker save
+
 
 
 -- lualine ------------------------------------------------------------
@@ -43,6 +51,15 @@ require('lualine').setup({
     }
 })
 
+
+-- Nvim-Tree ----------------------------------------------------------
+--nnoremap <C-n> :NvimTreeToggle<CR>
+--nnoremap <leader>r :NvimTreeRefresh<CR>
+--nnoremap <leader>n :NvimTreeFindFile<CR>
+map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+map('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true})
+map('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true})
+
 -- options ------------------------------------------------------------
 opt.encoding = "utf-8" -- Set default encoding to UTF-8
 opt.incsearch = true -- Shows the match while typing
@@ -51,6 +68,4 @@ opt.shiftwidth = 4 -- Size of an indent
 opt.smartindent = true -- Insert indents automatically
 opt.cursorline = true
 opt.clipboard = "unnamedplus"
-
-
 
