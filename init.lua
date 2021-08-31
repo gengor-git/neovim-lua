c608a3ec64077d025285bec31557721ed0f33f0d
Martin Palmowski <martin.palmowski@arvato-scs.com>
2021-08-31 08:31:22 +0200

    Added telescope.

--- init.lua
+++ init.lua
@@ -23,9 +23,12 @@ require("paq") ({
     "kyazdani42/nvim-web-devicons",
     "ryanoasis/vim-devicons",
 
-    "kyazdani42/nvim-tree.lua",
+--    "kyazdani42/nvim-tree.lua",
 
-    "ixru/nvim-markdown", -- Markdown mode
+    "nvim-telescope/telescope.nvim",
+--    "nvim-treesitter/nvim-treesitter",
+
+--    "ixru/nvim-markdown", -- Markdown mode
 
     "nvim-lua/plenary.nvim", -- this is required for neogit
     "TimUntersberger/neogit", -- magit like extension for neovim
@@ -72,30 +75,52 @@ require('tabline').setup {}
 --nnoremap <C-n> :NvimTreeToggle<CR>
 --nnoremap <leader>r :NvimTreeRefresh<CR>
 --nnoremap <leader>n :NvimTreeFindFile<CR>
-map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true})
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true})
+--map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+--map('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true})
+--map('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true})
 --g.nvim_tree_side = 'right'
-g.nvim_tree_width = '40'
-g.nvim_tree_quit_on_open = 1
+--g.nvim_tree_width = '40'
+--g.nvim_tree_quit_on_open = 0
 --g.nvim_tree_disable_keybindings = 1
 
--- Neogit -------------------------------------------------------------
+-- neogit -------------------------------------------------------------
 local neogit = require('neogit')
 neogit.setup {}
 map('n', '<Leader>gs', ':Neogit<CR>', {noremap = true})
+
+-- telescope ----------------------------------------------------------
+require('telescope').setup{}
+map('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true})
+map('n', '<Leader>fg', ':Telescope live_grep<CR>', {noremap = true})
+map('n', '<Leader>fb', ':Telescope buffers<CR>', {noremap = true})
+map('n', '<Leader>fh', ':Telescope help_tags<CR>', {noremap = true})
+
+-- treesitter highlighting --------------------------------------------
+--[[require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "javascript", "json", "lua", "php", "python", "scss", "yaml", "typescript", "rst" },
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    --disable = { "c", "rust" },  -- list of language that will be disabled
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+]]--
 
 -- writing ------------------------------------------------------------
 require('orgmode').setup{}
 
 -- options ------------------------------------------------------------
 opt.encoding = "utf-8" -- Set default encoding to UTF-8
-opt.incsearch = true -- Shows the match while typing
 opt.expandtab = true -- Use spaces instead of tabs
 opt.shiftwidth = 4 -- Size of an indent
 opt.smartindent = true -- Insert indents automatically
 opt.cursorline = true
 opt.clipboard = "unnamedplus"
+opt.incsearch = true -- Shows the match while typing
 opt.hlsearch = true -- Highlight found searchresults
 opt.ignorecase = true
 opt.number = true
