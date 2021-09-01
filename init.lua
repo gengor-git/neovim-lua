dc78b79db32dfc01042838d06d444f1a15ee0857
Martin Palmowski <martin.palmowski@arvato-scs.com>
2021-09-01 08:16:44 +0200

    Reformatted indentation.

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ require("paq") ({
     "nvim-telescope/telescope.nvim",
     --"nvim-treesitter/nvim-treesitter", -- has problems with missing cc or gcc
 
---    "ixru/nvim-markdown", -- Markdown mode
+    --    "ixru/nvim-markdown", -- Markdown mode
 
     "neovim/nvim-lspconfig", -- for setting up language servers
     "hrsh7th/nvim-cmp", -- completion, requires sources
@@ -50,8 +50,8 @@ require("paq") ({
 
 -- gitsigns setup -----------------------------------------------------
 --[[require("gitsigns").setup({
-  numhl = true,
-  signcolumn = false,
+numhl = true,
+signcolumn = false,
 })--]]
 
 
@@ -126,26 +126,26 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
         },
         ['<Tab>'] = function(fallback)
-          if vim.fn.pumvisible() == 1 then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-          elseif luasnip.expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-          else
-            fallback()
-          end
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+            else
+                fallback()
+            end
         end,
         ['<S-Tab>'] = function(fallback)
-          if vim.fn.pumvisible() == 1 then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-          elseif luasnip.jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-          else
-            fallback()
-          end
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+            elseif luasnip.jumpable(-1) then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+            else
+                fallback()
+            end
         end,
     },
     sources = {
